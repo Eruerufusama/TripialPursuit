40dcef5d01a78d28655a20237271a4e854267282
Eruerufusenpai <martin.faeroy@gmail.com>
2021-05-06 11:40:47 +0200

    Allowed heroku to host website

--- wiki_quiz/wiki_quiz/settings.py
+++ wiki_quiz/wiki_quiz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%gykw!dz#cb6teq58(^6nxbmf*2)*^z$7o80eik-u@0%ym)l68'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://frozen-gorge-23901.herokuapp.com/']
+ALLOWED_HOSTS = ['frozen-gorge-23901.herokuapp.com']
 
 
 # Application definition
